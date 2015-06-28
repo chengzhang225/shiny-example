26f9ad3aa72af6c8ed639f8072c505a7a67da6e9
chengzhang225 <cchang1212@163.com>
2015-06-28 20:56:59 +0800

    Create ui.R

---
+++ ui.R
@@ -0,0 +1,14 @@
+#ui.R
+
+shinyUI(pageWithSidebar(
+  headerPanel("Minimal example"),
+  sidebarPanel(
+    textInput(inputId = "comment",
+              label = "Say something?",
+              value = "")
+    ),
+  mainPanel(
+    h3("This is you saying it"),
+    textOutput("textDisplay")
+      )
+    ))
